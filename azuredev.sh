4be73018f7295d4a060dd73f3eefb5cb8f996224
landasan teori <landasanteori00@gmail.com>
2024-10-10 08:59:40 +0700

    -

--- azuredev.sh
+++ azuredev.sh
@@ -12,11 +12,11 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
 
 # Update package list and install the Azure CLI
 apt-get update
-apt-get install -y azure-cli terraform
+apt-get install -y azure-cli
 
 az --version
 
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 apt update 
 apt install -y terraform
