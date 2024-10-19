1ee2c25203063595d3f2dcc81d1582d016fcd3c4
landasanteori <landasanteori00@gmail.com>
2024-10-19 17:30:40 +0700

    satria

--- setuplaravel.sh
+++ setuplaravel.sh
@@ -6,8 +6,11 @@ apt install php8.3 php8.3-{cli,common,curl,mbstring,xml,sqlite3} -y
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 apt install composer -y
-
+apt install nodejs npm -y
 composer create-project --prefer-dist laravel/laravel laravelapp
 cd laravelapp
-php artisan serve --host=0.0.0.0 --port=8000
+php artisan key:generate
+php artisan 
+
+
 
