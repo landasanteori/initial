f79f718d93310995379d8bc56186200708a8c14a
satria <root@DESKTOP-C3ES73E.>
2024-10-19 09:06:20 +0700

    satria

--- setuplaravel.sh
+++ setuplaravel.sh
@@ -1,10 +1,11 @@
 apt update 
 add-apt-repository ppa:ondrej/php -y
 apt update
-apt install php8.3 php8.3-{cli,common,curl,mbstring,xml,sqlite3}
+apt install php8.3 php8.3-{cli,common,curl,mbstring,xml,sqlite3} -y
 
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
+apt install composer -y
 
 composer create-project --prefer-dist laravel/laravel laravelapp
 cd laravelapp
