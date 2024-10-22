b77ac7cf7c0de4988b4d0c24f4c00cd7e336d233
satria <root@DESKTOP-C3ES73E.>
2024-10-22 10:34:14 +0700

    satria update

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt install openjdk-17-jdk -y
+apt install openjdk-17-jdk openjdk-11-jdk -y
 
 wget -O /usr/share/keyrings/jenkins-keyring.asc \ https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \ https://pkg.jenkins.io/debian-stable binary/ | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
