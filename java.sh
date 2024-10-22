aa2cfb152daa12b697a0a66ec324493e1fdf1fce
satria <root@DESKTOP-C3ES73E.>
2024-10-22 10:46:51 +0700

    satria update

--- java.sh
+++ java.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Perbarui daftar paket dan sistem
-sudo apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
 
 # Instal OpenJDK 17
-sudo apt install openjdk-17-jdk -y
+apt install openjdk-17-jdk -y
 
 # Verifikasi instalasi
 java -version
 
 # Setel variabel lingkungan JAVA_HOME
-echo "JAVA_HOME=\"/usr/lib/jvm/java-17-openjdk-amd64\"" | sudo tee -a /etc/environment
+echo "JAVA_HOME=\"/usr/lib/jvm/java-17-openjdk-amd64\"" | tee -a /etc/environment
 source /etc/environment
 
 # Verifikasi variabel lingkungan
