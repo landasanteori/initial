59d74d654ff244fb8a8cab336d849dde7afed0e9
landasanteori <landasanteori00@gmail.com>
2024-10-24 21:22:11 +0700

    Update install-azurecli-terraform-suse.sh

--- install-azurecli-terraform-suse.sh
+++ install-azurecli-terraform-suse.sh
@@ -1,1 +1,12 @@
+sudo zypper refresh
+sudo zypper addrepo https://download.opensuse.org/repositories/systemsmanagement:terraform/openSUSE_Tumbleweed/systemsmanagement:terraform.repo
+sudo zypper refresh
+sudo zypper -n install azure-cli
+sudo zypper -n install terraform
+
+
+
+
+
+
 
